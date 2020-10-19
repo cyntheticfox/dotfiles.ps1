dab23932f9cbc3ad59373f5aafc948866b9363d7
David Houston <houstdav000@gmail.com>
2020-10-19 10:57:29 -0400

    Add installation powershell script

---
+++ install.ps1
@@ -0,0 +1,34 @@
+# install.ps1
+
+param([bool]$Force, [bool]$Verbose)
+
+# Find all files
+$ParentPath = Resolve-Path -Path '.'
+$DestPath = Resolve-Path -Path '~'
+$Excludes = @('LICENSE', 'README.md', 'install.ps1')
+
+foreach ($file in (Get-ChildItem -Path $ParentPath -Recurse -File -Exclude $Excludes)) {
+    $RelativePath = Resolve-Path -Path $file.FullName -Relative
+    $DestinationPath = Join-Path -Path $DestPath -ChildPath $RelativePath
+    
+    # If the file exists, and verbose is set, then inform the user
+    if ((Test-Path -Path $DestinationPath) -and $Verbose) {
+        Write-Host ('File exists: ' + $DestinationPath)
+    }
+    
+    # Don't overwrite files unless forced to
+    if ((-not (Test-Path -Path $DestinationPath)) -or $Force) {
+        if ($Verbose) {
+            Write-Host ('Writing: ' + $DestinationPath)
+        }
+
+        # Create parent directories if they don't exist
+        $ParentDir = (Split-Path -Path $DestinationPath -Parent)
+        If (-not (Test-Path -Path $ParentDir)) {
+            New-Item -Path $ParentDir -ItemType 'Directory'
+        }
+
+        # Write files forcefully
+        Copy-Item -Path $file -Destination $DestinationPath -Force
+    }
+}
