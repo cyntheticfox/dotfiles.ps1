70e55ed0ee461dd3a2bcd2f4969b968ce20ea700
David Houston <houstdav000@gmail.com>
2021-06-25 07:47:39 -0400

    Add pre-commit hooks
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- install.ps1
+++ install.ps1
@@ -5,18 +5,18 @@ param([switch]$Force, [switch]$Verbose)
 # Find all files
 $ParentPath = Resolve-Path -Path '.'
 $DestPath = Resolve-Path -Path '~'
-$Excludes = @('LICENSE', 'README.md', 'install.ps1', '.gitattributes', '.git')
+$Excludes = @('LICENSE', 'README.md', 'install.ps1', '.gitattributes', '.git','.pre-commit-config.yaml')
 
-foreach ($file in ((Get-ChildItem -Path $ParentPath -Recurse -File -Exclude $Excludes) + 
+foreach ($file in ((Get-ChildItem -Path $ParentPath -Recurse -File -Exclude $Excludes) +
                    (Get-ChildItem -Path $ParentPath -Recurse -File -Exclude $Excludes -Hidden))) {
     $RelativePath = Resolve-Path -Path $file.FullName -Relative
     $DestinationPath = Join-Path -Path $DestPath -ChildPath $RelativePath
-    
+
     # If the file exists, and verbose is set, then inform the user
     if ((Test-Path -Path $DestinationPath) -and $Verbose) {
         Write-Host ('File exists: ' + $DestinationPath)
     }
-    
+
     # Don't overwrite files unless forced to
     if ((-not (Test-Path -Path $DestinationPath)) -or $Force) {
         if ($Verbose) {
