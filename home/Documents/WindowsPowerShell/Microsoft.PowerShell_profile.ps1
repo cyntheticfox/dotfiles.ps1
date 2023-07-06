f271aaf10af1683dd0529f194ab09e0e4184e7f9
Cynthia Fox <cyntheticfox@gh0st.sh>
2023-07-05 21:07:12 -0400

    Profile: Add profile manipulation

--- home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,64 +1,193 @@
-# Set Environment Variables
-$ENV:SH = 'PS'
+### Remove built-in alisases
+Get-ChildItem 'alias:' | Remove-Item -Force
 
-# Create some basic aliases
-New-Alias -Name 'gpg' -Value 'C:\Program Files (x86)\GnuPG\bin\gpg.exe'
+### Make actually readable env vars
+$unreadableVars = Get-ChildItem 'env:*(*)*'
+
+foreach ($var in $unreadableVars) {
+  $newName = $var.Name.Replace('(', '_').Replace(')', '')
+
+  if (-not (Test-Path "env:$newName")) {
+    $null = New-Item "$env:$newName" -Value $var.Value
+  }
+}
+
+# Set PATH var
+$newPaths = @("$env:AppData\Python\**\Scripts")
+
+foreach ($newPath in $newPaths) {
+  if (Test-Path $newPath) {
+    $null = Set-Item 'env:Path' -Value "$env:Path;$(Resolve-Path $newPath)"
+  }
+}
+
+$drivesToAdd = @(
+  @{
+    Name = 'HKCR'
+    PSProvider = 'Registry'
+    Root = 'HKEY_CLASSES_ROOT'
+  }
+)
+
+$driveNames = (Get-PSDrive).Name
+
+foreach ($driveToAdd in $drivesToAdd) {
+  if ($driveToAdd.Name -notin $driveNames) {
+    $null = New-PSDrive @driveToAdd
+  }
+}
+
+# Add local module dir
+$moduleDir = "$env:UserProfile\Documents\WindowsPowerShell\Modules"
+
+if ($PSModulePath -notcontains $moduleDir) {
+  $null = Set-Item 'env:PSModulePath' -Value "$moduleDir;$env:PSModulePath"
+}
+
+# Set Window Title
+$Host.UI.RawUI.WindowTitle = "WindowsPowerShell $($Host.Version.Major)`.$($Host.Version.Minor)"
+
+function Prompt {
+  # Check if running as Admin
+  $principal = [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())
+  $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+  $userModule = "$env:USERDOMAIN\$env:USERNAME "
+  $hostModule = "on <$($env:COMPUTERNAME)> "
+  $pathModule = "in $(Get-Location)"
+  $promptChar = ''
+
+  if (Test-Path 'variable:/PSDebugContext' ) {
+    $promptChar = '?'
+  } elseif ($principal.IsInRole($adminRole)) {
+    $promptChar = '#'
+  } else {
+    $promptChar = '>'
+  }
+
+  $promptModule = "`r`nPS $promptChar"
+
+  return "`r`n$userModule$hostModule$pathModule$promptModule"
+}
+
+if (Test-Path "$env:ProgramFiles\Git") {
+  $env:EDITOR = "$env:ProgramFiles\Git\usr\bin\vim.exe"
+
+  . $PSScriptRoot\functions\git.plugin.ps1
+
+  New-Alias 'git-bash' "$env:ProgramFiles\Git\git-bash.exe"
+  New-Alias 'git-cmd' "$env:ProgramFiles\Git\git-cmd.exe"
+
+  $null = @(
+    'diff',
+    'dos2unix',
+    'gpg',
+    'grep',
+    'head',
+    'less',
+    'od',
+    'patch',
+    'perl',
+    'scp',
+    'sed',
+    'sftp',
+    'ssh',
+    'ssh-keygen',
+    'tr',
+    'tail',
+    'tar',
+    'tig',
+    'uniq',
+    'unix2dos',
+    'vim',
+    'vimdiff',
+    'wc',
+    'xxd'
+  ).ForEach({ New-Alias $_ "$env:ProgramFiles\Git\usr\bin\$($_).exe"})
+} else {
+  $env:EDITOR = 'nodepad.exe'
+}
+
 New-Alias -Name 'edit' -Value 'Code'
-New-Alias -Name 'firefox' -Value 'C:\Program Files\Mozilla Firefox\firefox.exe'
-New-Alias -Name 'git-bash' -Value 'C:\Program Files\git\git-bash.exe'
-New-Alias -Name 'touch' -Value 'New-Item'
-New-Alias -Name 'which' -Value 'Get-Command'
-New-Alias -Name 'vscode' -Value 'code'
-New-Alias -Name 'vi' -Value 'vim'
-New-Alias -Name 'vim' -Value 'nvim'
 
-Set-Alias -Name 'l' -Value 'Get-ChildItem'
+# Alias common linux commands
+New-Alias 'cat' 'Get-Content'
+New-Alias 'cd' 'Set-Location'
+New-Alias 'cp' 'Copy-Item'
+New-Alias 'echo' 'Write-Host'
+New-Alias 'env' 'Get-Variable'
+New-Alias 'help' 'Get-Help'
+New-Alias 'history' 'Get-History'
+New-Alias 'kill' 'Stop-Process'
+New-Alias 'll' 'Get-ChildItem'
+New-Alias 'man' 'Get-Help'
+New-Alias 'mv' 'Move-Item'
+New-Alias 'popd' 'Pop-Location'
+New-Alias 'poweroff' 'Stop-Computer'
+New-Alias 'ps' 'Get-Process'
+New-Alias 'pushd' 'Push-Location'
+New-Alias 'pwd' 'Get-Location'
+New-Alias 'reboot' 'Restart-Computer'
+New-Alias 'rm' 'Remove-Item'
+New-Alias 'rmdir' 'Remove-Item'
+New-Alias 'set' 'Set-Variable'
+New-Alias 'sleep' 'Start-Sleep'
+New-Alias 'tee' 'Tee-Object'
+New-Alias 'touch' 'New-Item'
+New-Alias 'unzip' 'Expand-Archive'
+New-Alias 'which' 'Get-Command'
 
-# Set up Get-ChildItemColor
-if (Get-Module -ListAvailable -Name get-childitemcolor) {
-  Import-Module Get-ChildItemColor
+# Add back some good standard aliases
+New-Alias '%' 'ForEach-Object'
+New-Alisa '?' 'Where-Object'
+New-Alias 'ft' 'Format-Table'
+New-Alias 'fl' 'Format-List'
+New-Alias 'fw' 'Format-Wide'
+New-Alias 'gm' 'Get-Member'
+New-Alias 'measure' 'Measure-Object'
+New-Alias 'select' 'Select-Object'
+New-Alias 'sort' 'Sort-Object'
+New-Alias 'unique' 'Unique-Object'
+New-Alias 'where' 'Where-Object'
 
-  Set-Alias -Name 'ls' -Value 'Get-ChildItemColorFormatWide' -Option AllScope
+New-Alias 'nvim' 'vim'
+New-Alias 'vi' 'vim'
+
+New-Alias 'h' 'history'
+New-Alias 'l' 'ls'
+New-Alias 'v' 'vim'
+New-Alias '~' '_cd_home'
+
+function ls() {
+  Get-ChildItem @args | Format-Wide -AutoSize
 }
 
-# Function for la
 function la() {
-    (Get-ChildItem) + (Get-ChildItem -Hidden) + (Get-ChildItem -System)
+  Get-ChildItem -Attributes '!H,H,S' @args
 }
 
-# Git Functions (inspired by joseluisq/gitnow on github)
-function state() {
-  git status -s
+function acs() {
+  Get-Alias @args
 }
 
-function stage() {
-  git add .
+function sha256sum() {
+  (Get-FileHash -Algorithm 'SHA256' @args).Hash.ToLower()
 }
 
-function unstage() {
-  git reset .
+function md5sum() {
+  (Get-FileHash -Algorithm 'MD5' @args).Hash.ToLower()
 }
 
-function commit() {
-  git commit @args
+function pkill() {
+  Get-Process @args | Stop-Process
 }
 
-function pull() {
-  git pull @args
+function path() {
+  $env:PATH.Replace(';', "`r`n")
 }
 
-function push() {
-  git push @args
-}
-
-function github([string]$RepoName) {
-  $GithubPath = 'github:' + $RepoName
-  git clone --recurse-submodules -j8 $GithubPath
-}
-
-function github([string]$UserName, [string]$RepoName) {
-  $GitHubPath = 'github:' + $UserName + '/' + $RepoName
-  git clone --recurse-submodules -j8 $GithubPath
+function _cd_home() {
+  Set-Location $env:USERPROFILE @args
 }
 
 # Chocolatey profile
